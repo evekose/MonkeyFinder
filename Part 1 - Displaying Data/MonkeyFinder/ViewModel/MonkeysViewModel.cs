9709a33a1077d3b6ae4577ef9e5dad15ea09e6c5
Eve <evekose@gmail.com>
2023-05-30 16:32:03 +0300

    Refresh View

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -17,6 +17,9 @@ public partial class MonkeysViewModel : BaseViewModel
 		this.geolocation = geolocation;
 	}
 
+	[ObservableProperty]
+	bool isRefreshing;
+
 	[RelayCommand]
 	async Task GetClosestMonkeyAsync()
 	{
@@ -103,6 +106,7 @@ public partial class MonkeysViewModel : BaseViewModel
 		finally
 		{
 			IsBusy = false;
+			IsRefreshing= false;
 		}
 	}
 }
